db6f41e4f1aa505d032ce99461d38218d40a26cc
TANITOLUWA IFEGBESAN <101012325+Tani1964@users.noreply.github.com>
2024-11-15 22:03:02 +0000

    db connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config()
 app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -6,6 +8,9 @@ app.use(express.urlencoded({extended:false}))
 app.listen(3000,()=>{
     console.log('server is running on port 3000')
 })
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected!'));
 
 app.get('/',(req, res)=>{
     res.json({'message':'working'})
